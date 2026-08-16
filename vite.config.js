63b780450275dba06de1f712bc1c2f4b3635e822
channsina <an.channsina.2824@rupp.edu.kh>
2026-08-16 14:26:02 +0700

    message update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import localImageUploadPlugin from './vite-plugins/local-image-upload.js'
 
 export default defineConfig({
-   base: "/Gen_Z_Store_Final/",
-  plugins: [react(), tailwindcss()],
+
+  plugins: [react(), tailwindcss(), localImageUploadPlugin()],
 })
